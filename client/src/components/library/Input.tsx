f5bcc92364ea3097f8e289fff1d50f4509689427
yeeun62 <byebye62@naver.com>
2022-04-11 17:00:30 +0900

    [Del]jsonform

--- client/src/components/library/Input.tsx
+++ client/src/components/library/Input.tsx
@@ -1,13 +1,5 @@
 import styled from "styled-components";
 import { Padding } from "../../recycleStyle";
-import { useState } from "react";
-import { JsonForms } from "@jsonforms/react";
-import {
-  materialRenderers,
-  materialCells,
-} from "@jsonforms/material-renderers";
-import schema from "../../schema.json";
-import uischema from "../../uischema.json";
 
 const InputWrap = styled.div`
   width: 100%;
@@ -30,20 +22,11 @@ const FormWrap = styled(Padding)`
     }
   }
 `;
-
-const Temporary = styled(Padding)``;
-
 interface inputProps {
   canvasState: any;
 }
 
 function Input({ canvasState }: inputProps): React.ReactElement {
-  const initialData = {
-    name: "이름을 입력해 주세요.",
-  };
-
-  const [data, setData] = useState(initialData);
-
   return (
     <InputWrap>
       <FormWrap>
@@ -51,16 +34,6 @@ function Input({ canvasState }: inputProps): React.ReactElement {
           <button type="button">인풋 삽입</button>
         </form>
       </FormWrap>
-      <Temporary>
-        <JsonForms
-          schema={schema}
-          uischema={uischema}
-          data={data}
-          renderers={materialRenderers}
-          cells={materialCells}
-          // onChange={({ data, _errors }) => setData(data)}
-        />
-      </Temporary>
     </InputWrap>
   );
 }
